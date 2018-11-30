94a4f799c7b5b236228cc4301aa3f6b215a15216
Charishma D'Souza <CharishmaD@asos.com>
2018-11-30 10:59:06 +0000

    remove extra setting,kts

--- .teamcity/Build/settings.kts
+++
@@ -1,7 +0,0 @@
-package Build
-
-import jetbrains.buildServer.configs.kotlin.v2018_1.project
-import jetbrains.buildServer.configs.kotlin.v2018_1.version
-
-version = "2018.1"
-project(Build.Project)
