6721069aeff3391dfbfc8d291b3afda1841c76c4
Charishma D'Souza <CharishmaD@asos.com>
2018-11-30 16:12:31 +0000

    add new step to build steps

--- .teamcity/BuildStep/BuildTypes/RunBuild.kt
+++ .teamcity/BuildStep/BuildTypes/RunBuild.kt
@@ -2,7 +2,7 @@ package BuildStep.BuildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2018_1.BuildType
 import jetbrains.buildServer.configs.kotlin.v2018_1.DslContext
-import jetbrains.buildServer.configs.kotlin.v2018_1.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2018_1.buildSteps.powerShell
 import jetbrains.buildServer.configs.kotlin.v2018_1.triggers.vcs
 
 object RunBuild : BuildType({
@@ -12,22 +12,40 @@ object RunBuild : BuildType({
         root(DslContext.settingsRoot)
     }
 
-    steps {
-        script {
-            name = "set version using script opa"
-            scriptContent = """
-                        #!/bin/bash
-                        HASH=%build.vcs.number%
-                        SHORT_HASH=${"$"}{HASH:0:7}
-                        BUILD_COUNTER=%build.counter%
-                        BUILD_NUMBER="1.0.${"$"}BUILD_COUNTER.${"$"}SHORT_HASH"
-                        echo "##teamcity[buildNumber '${"$"}BUILD_NUMBER']"
-                    """.trimIndent()
-        }
+
+        steps {
+            powerShell {
+                name = "Agent Diagnostics Step"
+                scriptMode = script {
+                    content = """
+                    dir "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\"
+                    dir "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools"
+                    Get-ChildItem Env:
+                    dir "C:\TeamCity\tools\NuGet.CommandLine.3.4.4\"
+                """.trimIndent()
+                }
+            }
+            powerShell {
+                name = "Set build number"
+                scriptMode = script {
+                    content = """
+                    ${'$'}hash = "%build.vcs.number%"
+                    ${'$'}shortHash = ${'$'}hash.substring(0, 7)
+                    ${'$'}buildCounter = "%build.counter%"
+                    ${'$'}buildNumber = "1.0.${'$'}buildCounter-v${'$'}shortHash"
+                    Write-Host "##teamcity[buildNumber '${'$'}buildNumber']"
+                    echo "##teamcity[buildNumber '${"$"}BUILD_NUMBER'}"
+                """.trimIndent()
+                }
+            }
     }
 
     triggers {
         vcs {
+            branchFilter = ""
+            perCheckinTriggering = true
+            groupCheckinsByCommitter = true
+            enableQueueOptimization = false
         }
     }
 })
